8ddc862d54acccf2eb09c1a0ed8b40d22fc86371
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-30 23:12:44 -0500

    move test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+/*!
+ * update <http://github.com/helpers/update>
+ *
+ * Copyright (c) 2013-2015, Jon Schlinkert.
+ * Licensed under the MIT license.
+ */
+
+var assert = require('assert');
+var update = require('./');
+
+describe('update', function () {
+  it('should update the year', function () {
+    assert.equal(update('Copyright (c) 2013, Jon Schlinkert.'), 'Copyright (c) 2013-2015, Jon Schlinkert.');
+  });
+});
+
