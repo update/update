76aad4c912bb4c51390aab45ed13d6117f142e28
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-19 12:18:31 -0500

    rename

---
+++ plugins/verbmd.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var matter = require('gray-matter');
+var through = require('through2');
+var verbmd = require('../lib/verbmd');
+var logger = require('../lib/logging');
+
+module.exports = function verbmdPlugin(verb) {
+  return function () {
+    return through.obj(function (file, enc, cb) {
+      if (file.isNull() || !file.isBuffer()) {
+        this.push(file);
+        return cb();
+      }
+
+      if (file.path.indexOf('verbfile')) {
+        var str = file.contents.toString();
+        var log = logger(str);
+
+        var obj = matter(str);
+        var keys = Object.keys(obj.data);
+
+        if (keys.length && obj.data.hasOwnProperty('tags')) {
+          str = obj.content.replace(/^\s+/, '');
+          log.success(str, 'stripped front-matter in', file.relative);
+        }
+
+        var year = verb.get('data.copyright') || new Date().getFullYear();
+        str = verbmd(str, year);
+
+        log.success(str, 'updated helpers in', file.relative);
+        file.contents = new Buffer(str);
+      }
+
+      this.push(file);
+      cb();
+    });
+  };
+};
