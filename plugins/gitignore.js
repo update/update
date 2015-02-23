b799a2f9d5690fd1ee97901938e1af412fe69e5c
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-22 21:18:46 -0500

    adds basic logic to update gitignore

--- plugins/gitignore.js
+++ plugins/gitignore.js
@@ -1,27 +1,46 @@
 'use strict';
 
+var difference = require('arr-diff');
 var through = require('through2');
 var utils = require('../lib/utils');
 var logger = require('../lib/logging');
 
-module.exports = function dotfilesPlugin() {
+module.exports = function gitignore() {
   return through.obj(function (file, enc, cb) {
     if (file.isNull() || !file.isBuffer()) {
       this.push(file);
       return cb();
     }
 
-    var str = file.contents.toString();
-    console.log(str)
-    var log = logger(str);
+    if (utils.contains(file, '.gitignore')) {
+      // just get these started
+      var patterns = ['*.DS_Store'];
 
-    // if (utils.contains(file, '.gitattributes')) {
-    //   str = '*.* text';
-    //   log.success(str, 'updated patterns in', file.relative);
-    // }
+      var str = file.contents.toString();
+      var log = logger(str);
 
-    file.contents = new Buffer(str);
+      var lines = str.split('\n').map(trim);
+      var diff = difference(patterns, lines);
+
+      if (lines[0].charAt(0) === '#') {
+        var comment = lines.slice(0, 1);
+        var rest = lines.slice(1);
+        lines = comment.concat(diff).concat(rest);
+      } else {
+        lines = diff.concat(lines);
+      }
+
+      str = lines.join('\n');
+      log.success(str, 'updated patterns in', file.relative);
+      file.contents = new Buffer(str);
+    }
+
     this.push(file);
     cb();
   });
 };
+
+
+function trim(str) {
+  return str.trim();
+}
