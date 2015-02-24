4015c09c2c027050d1e1cee3b0f07e19d27e8fe8
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-24 00:27:14 -0500

    only read tests

--- plugins/tests.js
+++ plugins/tests.js
@@ -4,7 +4,7 @@ var through = require('through2');
 var logger = require('../lib/logging');
 var tests = require('../lib/tests');
 
-module.exports = function testsPlugin(verb) {
+module.exports = function(verb) {
   return function() {
     return through.obj(function (file, enc, cb) {
       if (file.isNull() || !file.isBuffer()) {
@@ -12,18 +12,26 @@ module.exports = function testsPlugin(verb) {
         return cb();
       }
 
-      var str = file.contents.toString();
-      var log = logger(str);
+      if (utils.contains(file.path, 'test')) {
+        var str = file.contents.toString();
+        var log = logger(str);
 
-      var hasShould = /['"]should/.test(str);
-      verb.set('data.hasShould', hasShould);
+        var hasShould = /['"]should/.test(str);
+        verb.set('data.hasShould', hasShould);
 
-      if (hasShould) {
-        str = tests.fixShould(str, file.relative);
-        log.success(str, 'updated "should" statements in', file.relative);
+        // `should` exists in test files
+        if (hasShould) {
+          str = tests.fixShould(str, file.relative);
+          log.success(str, 'updated "should" statements in', file.relative);
+        }
+
+        var deps = verb.env.devDependencies;
+        if (hasShould && !deps.hasOwnProperty('should')) {
+          console.log(verb.env);
+        }
+        file.contents = new Buffer(str);
       }
 
-      file.contents = new Buffer(str);
       this.push(file);
       cb();
     });
