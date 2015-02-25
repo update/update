28badbc689dbc01c32946802660e861b2e14558e
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 07:57:58 -0500

    clean up

--- test.js
+++ test.js
@@ -1,17 +1,25 @@
 /*!
  * update <https://github.com/jonschlinkert/update>
  *
- * Copyright (c) 2013-2015, Jon Schlinkert.
+ * Copyright (c) 2015, Jon Schlinkert.
  * Licensed under the MIT License.
  */
 
+'use strict';
+
 var assert = require('assert');
+require('should');
 var update = require('./');
 
 describe('update', function () {
-  it('should update the year', function () {
-    assert.equal(update('Copyright (c) 2013, Jon Schlinkert.'), 'Copyright (c) 2013-2015, Jon Schlinkert.');
-    assert.equal(update('Copyright (c) 2014, Jon Schlinkert.'), 'Copyright (c) 2014-2015, Jon Schlinkert.');
-    assert.equal(update('Copyright (c) 2015, Jon Schlinkert.'), 'Copyright (c) 2015, Jon Schlinkert.');
+  it('should:', function () {
+    update('a').should.equal({a: 'b'});
+    update('a').should.eql('a');
+  });
+
+  it('should throw an error:', function () {
+    (function () {
+      update();
+    }).should.throw('update expects valid arguments');
   });
 });
