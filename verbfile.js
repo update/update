93baa329d235b2c3ec4712f1c50d498e82079e57
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 07:57:42 -0500

    add tasks

--- verbfile.js
+++ verbfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var fs = require('fs');
 var del = require('del');
 var path = require('path');
 var verb = require('verb');
@@ -7,32 +8,43 @@ var parse = require('parse-copyright');
 var log = require('./lib/logging')({nocompare: true});
 var plugins = require('./plugins')(verb);
 var utils = require('./lib/utils');
+var glob = require('glob');
 
-verb.known(function(env) {
-  var orgs = ['jonschlinkert', 'doowb', 'assemble', 'verb', 'helpers', 'regexps'];
-  var repo = env.repository;
-  if (typeof repo === 'object') {
-    repo = repo.url;
-  }
-  var len = orgs.length;
-  while(len--) {
-    var ele = orgs[len];
-    if (repo.indexOf(ele) !== -1) {
-      return true;
-    }
-  }
-  return false;
-});
+function tryReaddir(fp) {
+  try {
+    return fs.readdirSync(fp);
+  } catch (err) {}
+  return [];
+}
 
-verb.data({
-  travis: {
-    sudo: false,
-    language: 'node_js',
-    node_js:  ['iojs', '0.12', '0.10']
-  }
-});
+// verb.known(function(env) {
+//   var orgs = ['jonschlinkert', 'doowb', 'assemble', 'verb', 'helpers', 'regexps'];
+//   var repo = env.repository;
+//   if (typeof repo === 'object') {
+//     repo = repo.url;
+//   }
+//   var len = orgs.length;
+//   while(len--) {
+//     var ele = orgs[len];
+//     if (repo.indexOf(ele) !== -1) {
+//       return true;
+//     }
+//   }
+//   return false;
+// });
 
 verb.onLoad(/./, function (file, next) {
+  var files = tryReaddir(process.cwd());
+  var tests = [];
+
+  if (files.indexOf('test') !== -1) {
+    tests = tryReaddir(process.cwd() + '/test');
+  }
+
+  verb.set('stats.files', files.concat(tests || []));
+  verb.set('stats.hasTravis', fs.existsSync('.travis.yml'));
+  verb.match = utils.match(files);
+
   file.render = false;
   file.readme = false;
   next();
@@ -49,6 +61,21 @@ verb.copy('.verbrc.md', function (file) {
   return path.dirname(file.relative);
 });
 
+var files = glob.sync('test/**').filter(function (fp) {
+  return fs.statSync(fp).isDirectory();
+});
+
+var singleTest = false;
+if (files && files.length === 1) {
+  singleTest = true;
+  verb.set('singleTest', true);
+  verb.copy('test/test.js', function (file) {
+    file.path = 'test.js';
+    log.success('moved', file.path);
+    return file.base;
+  });
+}
+
 verb.copy('LICENSE-MIT', function (file) {
   file.path = 'LICENSE';
   log.success('renamed', file.relative);
@@ -57,7 +84,6 @@ verb.copy('LICENSE-MIT', function (file) {
 
 verb.task('banners', function () {
   verb.src(['*.js', 'test/*.js', 'lib/*.js'], {render: false})
-    .pipe(plugins.tests())
     .pipe(plugins.banners())
     .pipe(verb.dest(function (file) {
       return path.dirname(file.path);
@@ -91,16 +117,15 @@ verb.task('travis', function () {
     }));
 });
 
-verb.task('license', function () {
-  verb.src('LICENSE{,-MIT}', {render: false})
-    .pipe(plugins.license())
+verb.task('tests', function () {
+  verb.src(['test.js', 'test/*.js'], {render: false})
+    .pipe(plugins.tests())
     .pipe(verb.dest(function (file) {
-      file.path = 'LICENSE';
       return path.dirname(file.path);
     }));
 });
 
-verb.task('travis', function () {
+verb.task('license', function () {
   verb.src('LICENSE{,-MIT}', {render: false})
     .pipe(plugins.license())
     .pipe(verb.dest(function (file) {
@@ -111,14 +136,20 @@ verb.task('travis', function () {
 
 verb.task('dotfiles', function () {
   verb.src('.git*', {render: false, dot: true})
-    .pipe(plugins.dotfiles())
+    .pipe(plugins.editorconfig())
     .pipe(plugins.gitignore())
     .pipe(verb.dest(function (file) {
       return path.dirname(file.path);
     }))
     .on('end', function (cb) {
       var files = ['.npmignore', 'test/mocha.opts', '.verbrc.md', 'LICENSE-MIT'];
-      var exists = utils.exists(files).EXISTS;
+      var res = utils.exists(files);
+
+      var exists = res.EXISTS;
+      if (verb.get('singleTest')) {
+        exists.push('test');
+      }
+
       if (exists.length) {
         del(exists, cb);
         log.info('deleted', exists.join(', '));
@@ -134,18 +165,22 @@ verb.task('pkg', function () {
 
 verb.task('readme', function () {
   verb.src('.verb.md')
-    .pipe(verb.dest('.'));
+    .pipe(verb.dest('.'))
+    .on('end', function (cb) {
+      log.success(true, 'updated.');
+    });
 });
 
 verb.task('default', [
+  'tests',
   'banners',
   'verbfile',
   'dotfiles',
   'travis',
   'jshint',
   'license',
-  'pkg'
+  'pkg',
+  'readme'
 ]);
-
-// verb.diff()
+verb.diff()
 verb.run();
