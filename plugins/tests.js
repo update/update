73f5a73b30c8bfd145b973dad498a2b94e181f42
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-28 23:53:15 -0500

    clean up/externalize test junk

--- plugins/tests.js
+++ plugins/tests.js
@@ -2,13 +2,13 @@
 
 var gutil = require('gulp-util');
 var through = require('through2');
-var regex = require('requires-regex');
 var logger = require('../lib/logging');
-var tests = require('../lib/tests');
+var should = require('../lib/tests/should');
+var paths = require('../lib/tests/paths');
 var utils = require('../lib/utils');
 
-module.exports = function(verb) {
-  return function() {
+module.exports = function (verb) {
+  return function () {
     return through.obj(function (file, enc, cb) {
       if (file.isNull() || !file.isBuffer()) {
         this.push(file);
@@ -16,42 +16,16 @@ module.exports = function(verb) {
       }
 
       try {
-        if (utils.contains(file.path, 'test')) {
-          var str = file.contents.toString();
-          var log = logger(str);
+        if (verb.file('test')) {
+          // fix `should` related info
+          file = should(file, verb);
 
-          // test.js is at the root, let's make sure
-          // the path to `index.js` is correct
-          if (file.path.indexOf('test/') === -1) {
-            str.replace(regex(), function ($1, $2, $3, fp) {
-              if (fp && fp === '../' || fp === '..') {
-                str = str.replace(fp, './');
-              }
-            });
-          }
-
-          var hasShould = /['"]should/.test(str);
-          verb.set('data.hasShould', hasShould);
-
-          var deps = verb.env.devDependencies;
-
-          // `should` exists in test files
-          if (hasShould && deps) {
-            str = tests.fixShould(str, file.relative);
-            if (!deps.hasOwnProperty('should')) {
-              // TODO: add should to deps if exists in tets
-              // console.log(verb.env);
-            }
-
-            log.success(str, 'updated "should" statements in', file.relative);
-          } else if (deps && deps.hasOwnProperty('should')) {
-            verb.set('strip.pkg.devDependencies', 'should');
-          }
-
-          file.contents = new Buffer(str);
+          // fix paths
+          file = paths(file, verb);
         }
+
       } catch (err) {
-        console.log(err);
+        console.error('update:tests', err);
         this.emit('error', new gutil.PluginError('update:tests', err));
         return cb();
       }
