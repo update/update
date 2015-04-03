15c041b764b75e75f6963e79a33c00656360eb58
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-02 21:22:50 -0400

    remove junk

--- transforms/start.js
+++ transforms/start.js
@@ -1,20 +1,12 @@
 'use strict';
 
-var fs = require('fs');
-var del = require('del');
-var path = require('path');
-var verb = require('verb');
-var glob = require('glob');
-var gutil = require('gulp-util');
-var parse = require('parse-copyright');
-
-var logger = require('../lib/logging');
-var plugins = require('../plugins')(verb);
+var debug = require('debug')('update:transform');
 var verbmd = require('../plugins/readme/verbmd');
 var utils = require('../lib/utils');
-var log = logger({nocompare: true});
 
 module.exports = function (verb) {
+  debug('transform: start');
+
   verb.set('stats.hasTravis', verb.exists('.travis.yml'));
   var verbfile = verb.files('.verb*');
 
