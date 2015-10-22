566a8b09c918dce4ec3ad9853984a0adf1e6613b
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-22 19:49:16 -0400

    get everything working

--- bin/update.js
+++ bin/update.js
@@ -4,14 +4,16 @@ var argv = require('minimist')(process.argv.slice(2));
 var updaters = require('../lib/updaters');
 var utils = require('../lib/utils');
 
-updaters(argv).register('update-*', {
+var update = updaters(argv);
+
+update.register('update-*', {
   cwd: utils.gm
 });
 
-updaters.base.task('run', function (cb) {
-  updaters.run(cb);
+update.base.task('run', function (cb) {
+  update.run(cb);
 });
 
-updaters.base.build('default', function (err) {
+update.base.build('default', function (err) {
   if (err) console.error(err);
 });
