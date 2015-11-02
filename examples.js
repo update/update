5e6fbb0fd908b0e889baeca11ee34a65a3f89543
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-02 03:07:15 -0500

    lint

--- examples.js
+++ examples.js
@@ -1,30 +1,59 @@
 'use strict';
 
+var path = require('path');
+var del = require('rimraf');
 var through = require('through2');
 var update = require('./');
-var del = require('del');
-var app = update()
-  .use(require('./pipeline'))
+var app = update();
 
-app.plugin('a', require('./lib/pipeline/a')());
-app.plugin('b', require('./lib/pipeline/b'));
-app.plugin('c', require('./lib/pipeline/c')());
+// app.config.get = function () {
+
+// };
+// console.log(app)
+
+
+// app.config({
+//   plugins: function (obj) {
+//     for (var key in obj) {
+//       var name = path.basename(key, path.extname(key));
+//       var fn = require(path.resolve(key));
+//       app.plugin(name, obj[key], fn);
+//     }
+//   }
+// });
+
+
+// app.config('plugins', function (obj) {
+//   for (var key in obj) {
+//     var name = path.basename(key, path.extname(key));
+//     var fn = require(path.resolve(key));
+//     app.plugin(name, obj[key], fn);
+//   }
+// });
+
+// app.config.process();
+
+// app.plugin('a', require('./lib/pipeline/a')());
+// app.plugin('b', require('./lib/pipeline/b'));
+// app.plugin('c', require('./lib/pipeline/c')());
 // app.plugin(/foo/, require('./lib/pipeline/d')());
 
-app.disable('plugin.c');
 
-app.task('default', function (cb) {
-  app.src('*')
-    .on('error', console.log)
-    .pipe(through.obj(function (file, enc, next) {
-      if (file.isNull()) return next();
-      next(null, file);
-    }))
-    .pipe(app.pipeline())
-    .pipe(app.dest('actual'))
-    .on('finish', cb);
-});
+// app.on('error', function(err) {
+//   console.log('Error in plugin:', err.plugin, err.message)
+// });
 
-app.build('default', function(err) {
-  if (err) return console.log(err);
-});
+// app.task('default', function (cb) {
+//   app.src('LICENSE')
+//     .pipe(through.obj(function (file, enc, next) {
+//       if (file.isNull()) return next();
+//       next(null, file);
+//     }))
+//     .pipe(app.pipeline())
+//     .pipe(app.dest('actual'))
+//     .on('finish', cb);
+// });
+
+// app.build('default', function(err) {
+//   if (err) return console.log(err);
+// });
