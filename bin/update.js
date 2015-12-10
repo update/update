cd776be6b172deba0bf96e7e2ce33e302e92b214
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-10 13:23:58 -0500

    clean up

--- bin/update.js
+++ bin/update.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 var path = require('path');
-var stamp = require('time-stamp');
-var gray = require('ansi-gray');
 var Runner = require('../lib/runner/runner')();
 var utils = require('../lib/utils');
 var argv = require('minimist')(process.argv.slice(2), {
@@ -12,12 +10,14 @@ var argv = require('minimist')(process.argv.slice(2), {
 var cmd = utils.commands(argv);
 var runner = new Runner(argv);
 
+runner.base.option(argv);
+runner.option(argv);
+
 var task = cmd.list ? ['list', 'default'] : 'default';
 
-runner.on('*', function (method, key, val) {
+runner.on('*', function(method, key, val) {
   console.log(method + ':', key, val);
 });
-
 
 if (argv.verbose) {
   runner.on('register', function(key) {
@@ -27,16 +27,11 @@ if (argv.verbose) {
 
 runner.registerEach('update-*', {cwd: utils.gm});
 
-runner.base.task('run', function (cb) {
+runner.base.task('run', function(cb) {
   runner.run(cb);
 });
 
-runner.base.build(task, function (err) {
-  if (err) console.error(err);
-  timestamp('finished');
+runner.base.build(task, function(err) {
+  if (err) return console.error(err);
+  utils.timestamp('done ' + utils.green(utils.successSymbol));
 });
-
-function timestamp(msg) {
-  var time = ' ' + gray(stamp('HH:mm:ss.ms', new Date()));
-  return console.log(time, msg, utils.green(utils.successSymbol));
-}
